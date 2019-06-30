37b681e3bf232565309e17a80afd6377aa274246
Kaamil Jasani <kaamilj@yahoo.com>
2019-06-30 16:22:59 +0100

    Dependencies, entrypoints

---
+++ src/main/java/com/extracraftx/minecraft/vanillaendervators/VanillaEndervators.java
@@ -0,0 +1,11 @@
+package com.extracraftx.minecraft.vanillaendervators;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VanillaEndervators implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
